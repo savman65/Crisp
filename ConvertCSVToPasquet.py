34f293a90d8aab7ab990349c5127bbe87590b9e4
Matt Savitz <mattsavitz@mac-book-pro.local>
2024-06-19 21:21:51 -0400

    cleaning up

--- ConvertCSVToPasquet.py
+++ ConvertCSVToPasquet.py
@@ -9,23 +9,21 @@ import time
 
 storage_account_name = os.getenv('crispsa')
 storage_account_key = os.getenv('crispsakey')
-
-#for local testing
-#storage_account_key = ""
-#storage_account_name = "crispsadsavitz"
-
 source_container_name = "csv"
 destination_container_name = "parquet"
 local_target_directory = "./blob_files/"
 saPollingIntervalSeconds = 5
 connection_string = f"DefaultEndpointsProtocol=https;AccountName={storage_account_name};AccountKey={storage_account_key};EndpointSuffix=core.windows.net"
-#blobName = os.getenv('crispsablobname')
+
+#for local testing
+#storage_account_key = ""
+#storage_account_name = "crispsadsavitz"
 
 blobRecord = []
 blobsToProcessNow = []
 
-
-def download_blob(storage_account_name, storage_account_key, container_name, local_target_directory):
+#Download all blobs in the container
+def download_blobs(storage_account_name, storage_account_key, container_name, local_target_directory):
     # Construct the BlobServiceClient using the account key
     global blobRecord
     global blobsToProcessNow
@@ -38,22 +36,22 @@ def download_blob(storage_account_name, storage_account_key, container_name, loc
 
     blob_list = container_client.list_blobs()
 
-    # Download each blob to local directory
+    #for each blob in the container, if we haven't converted the blob to parquet yet, download the blob and add it to blobsToProcessNow
     for blob in blob_list:
-        blob_client = container_client.get_blob_client(blob)
-        download_file_path = local_target_directory + blob.name
-
-        print(f"Downloading blob to {download_file_path}")
-
-        with open(download_file_path, "wb") as download_file:
-            download_file.write(blob_client.download_blob().readall())
-
-        print(f"Downloaded {blob.name} to {download_file_path}")
-        
         if blob.name not in blobRecord:
             blobsToProcessNow.append(blob.name)
             blobRecord.append(blob.name)
-        print(f"blob recorded so far: {blobRecord}")
+
+            blob_client = container_client.get_blob_client(blob)
+            download_file_path = local_target_directory + blob.name
+
+            print(f"Downloading blob to {download_file_path}")
+
+            with open(download_file_path, "wb") as download_file:
+                download_file.write(blob_client.download_blob().readall())
+
+            print(f"Downloaded {blob.name} to {download_file_path}")
+        print(f"all blobs recorded so far: {blobRecord}")
         print(f"converting the following blobs to parquet now: {blobsToProcessNow}")
 
 def upload_blob_to_azure(connection_string, container_name, blob_name, parquet_file_path):
@@ -68,31 +66,22 @@ def upload_blob_to_azure(connection_string, container_name, blob_name, parquet_f
 
 if __name__ == "__main__":
     while True:
-        #try: 
-        download_blob(storage_account_name, storage_account_key, source_container_name, local_target_directory)
+        download_blobs(storage_account_name, storage_account_key, source_container_name, local_target_directory)
 
+        #for every blob in blobsToProcessNow, convert the blob to parquet and upload it to the storage account
         for blob in blobsToProcessNow:
             # Read CSV file into pandas DataFrame
             blobNoExt = blob.replace(".csv", "")
             df = pd.read_csv(f"{local_target_directory}/{blobNoExt}.csv")
 
-            # Specify the output Parquet file
-            #parquet_file = '{blobName}.parquet'
-
-            #pyarrow for Parquet writing
             df.to_parquet(f"{local_target_directory}/{blobNoExt}.parquet", engine='pyarrow')
-
-            # Option 2: Using fastparquet for Parquet writing
-            # Ensure to have fastparquet installed: pip install fastparquet
-            # df.to_parquet(parquet_file, engine='fastparquet')
 
             print(f"CSV file '{blobNoExt}.csv' converted to Parquet file '{blobNoExt}.parquet' successfully.")
 
             upload_blob_to_azure(connection_string, destination_container_name, f"{blobNoExt}.parquet", f"./{local_target_directory}/{blobNoExt}.parquet")
 
         blobsToProcessNow = []
-        #except Exception as e:
-        #    print("failed")
+        
         time.sleep(saPollingIntervalSeconds)
 
 
